33b740f4a72fc1745a5bf94473ef23192bfd24d6
BrentVanant1999 <bvancar@yahoo.com>
2018-08-03 00:32:35 -0500

    Bug fixed puzzle object initation and randomization methods -program working as intended -need to bugtest and comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,25 +9,31 @@ public class Main {
     private static final String puzzleSizeRangeError = "---Puzzle Size Range Error (3-10)---";
 
     //difficulty errors
-    private static final String puzzleDifficultyRangeError = "---Puzzle Difficulty Range Error (1-7)---";
+    private static final String puzzleDifficultyRangeError = "---Puzzle Difficulty Range Error (1-10)---";
 
     //amount errors
-    private static final String puzzleAmountRangeError = "---Puzzle Difficulty Range Error (1-1000)---";
+    private static final String puzzleAmountRangeError = "---Puzzle Amount to Generate Range Error (1-1000)---";
+
+    //colors erros
+    private static final String puzzleColorRangeError = "---Puzzle Colors Range Error (3-6)---";
 
     //prompts
-    private static final String puzzleSizePrompt = "Enter Puzzle Size in integer form:";
-    private static final String puzzleDifficultyPrompt = "Enter Difficulty Level (1-7):";
-    private static final String puzzleAmountPrompt = "Enter Amount of levels to generate:";
+    private static final String puzzleSizePrompt = "Enter Puzzle Size in integer form (3-10):";
+    private static final String puzzleDifficultyPrompt = "Enter Difficulty Level (1-10):";
+    private static final String puzzleAmountPrompt = "Enter Amount of Levels to generate (1-1000):";
+    private static final String puzzleColorPrompt = "Enter Amount of Colors in puzzle (3-6):";
 
     //constant format error
     private static final String formatError = "--!--Response Formatting Error--!--";
     //range constants
     private static final int MAX_PUZZLE_SIZE = 10;
     private static final int MIN_PUZZLE_SIZE = 3;
-    private static final int MAX_PUZZLE_DIFF = 7;
+    private static final int MAX_PUZZLE_DIFF = 10;
     private static final int MIN_PUZZLE_DIFF = 1;
     private static final int MAX_PUZZLE_AMOUNT = 1000;
     private static final int MIN_PUZZLE_AMOUNT = 1;
+    private static final int MAX_PUZZLE_COLOR = 6;
+    private static final int MIN_PUZZLE_COLOR = 3;
     /*
      * Prompts user through scanner for integer input.
      */
@@ -36,17 +42,21 @@ public class Main {
         boolean sizeCorrect = false;
         boolean difficultyCorrect = false;
         boolean amountCorrect = false;
+        boolean colorCorrect = false;
+
         int sizeVal;
         int difficultyVal;
         int amountVal;
+        int colorVal;
         Scanner mainScanner=new Scanner(System.in);
 
         sizeVal = promptUser(sizeCorrect, mainScanner, puzzleSizePrompt, MAX_PUZZLE_SIZE, MIN_PUZZLE_SIZE, puzzleSizeRangeError, formatError);
         difficultyVal = promptUser(difficultyCorrect, mainScanner, puzzleDifficultyPrompt, MAX_PUZZLE_DIFF, MIN_PUZZLE_DIFF, puzzleDifficultyRangeError, formatError);
         amountVal = promptUser(amountCorrect, mainScanner, puzzleAmountPrompt, MAX_PUZZLE_AMOUNT, MIN_PUZZLE_AMOUNT, puzzleAmountRangeError, formatError);
+        colorVal = promptUser(colorCorrect, mainScanner, puzzleColorPrompt, MAX_PUZZLE_COLOR, MIN_PUZZLE_COLOR, puzzleColorRangeError, formatError);
 
         for (int i =0;  i<amountVal; i++){
-            Puzzle currentPuzzle = new Puzzle(sizeVal, difficultyVal);
+            Puzzle currentPuzzle = new Puzzle(sizeVal, difficultyVal, colorVal);
             currentPuzzle.initiate();
             currentPuzzle.randomize();
             currentPuzzle.print();
--- src/com/company/Puzzle.java
+++ src/com/company/Puzzle.java
@@ -6,11 +6,14 @@ public class Puzzle {
     private Piece[][] contents;
     private int size;
     private int colorWheelSize;
+    private int difficulty;
     private Random myRandom;
-    Puzzle(int coreSize, int activePieces, int colorWheelSize) {
+    //main puzzle constructor
+    Puzzle(int size, int difficulty, int colorWheelSize) {
         this.myRandom = new Random();
-        this.size = coreSize;
+        this.size = size;
         this.colorWheelSize = colorWheelSize;
+        this.difficulty = difficulty;
         this.contents = new Piece[this.size][this.size];
     }
 
@@ -21,27 +24,31 @@ public class Puzzle {
         //populate contents with that number
         for (int i = 0; i < this.size; i++) {
             for (int j = 0; j < this.size; j++) {
-                this.contents[i][j].setColorVal(randomInt);
+                this.contents[i][j] = new Piece(randomInt);
             }
         }
     }
 
     //randomly stimulate the puzzle
     public void randomize(){
-        int randomXCord = myRandom.nextInt(this.size);
-        int randomYCord = myRandom.nextInt(this.size);
-        this.contents[randomXCord][randomYCord].colorSwitch(this.colorWheelSize);
-        if (randomXCord + 1 < this.size) {
-            this.contents[randomXCord+1][randomYCord].colorSwitch(this.colorWheelSize);
-        }
-        if (randomXCord - 1 > 0) {
-            this.contents[randomXCord-1][randomYCord].colorSwitch(this.colorWheelSize);
-        }
-        if (randomYCord + 1 < this.size) {
-            this.contents[randomXCord][randomYCord+1].colorSwitch(this.colorWheelSize);
-        }
-        if (randomYCord - 1 > 0) {
-            this.contents[randomXCord][randomYCord-1].colorSwitch(this.colorWheelSize);
+        System.out.println("---Randomizing Puzzle---");
+        for (int j = 0; j <difficulty; j ++) {
+            int randomXCord = myRandom.nextInt(this.size);
+            int randomYCord = myRandom.nextInt(this.size);
+            this.contents[randomXCord][randomYCord].colorSwitch(this.colorWheelSize);
+            if (randomXCord + 1 < this.size) {
+                this.contents[randomXCord + 1][randomYCord].colorSwitch(this.colorWheelSize);
+            }
+            if (randomXCord - 1 >= 0) {
+                this.contents[randomXCord - 1][randomYCord].colorSwitch(this.colorWheelSize);
+            }
+            if (randomYCord + 1 < this.size) {
+                this.contents[randomXCord][randomYCord + 1].colorSwitch(this.colorWheelSize);
+            }
+            if (randomYCord - 1 >= 0) {
+                this.contents[randomXCord][randomYCord - 1].colorSwitch(this.colorWheelSize);
+            }
+            System.out.println("Random center touch:" + randomXCord + "," + randomYCord);
         }
     }
 
