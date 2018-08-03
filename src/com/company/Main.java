c894a74c9507aaf85b1dcdd51577dcdd9e6db83f
BrentVanant1999 <bvancar@yahoo.com>
2018-08-02 23:31:55 -0500

    Finished user interface within main method.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,29 @@ import java.util.Scanner;
  * Holds the runner which handles getting user input and running the puzzle generation process.
  */
 public class Main {
-   private static final String puzzleSizeFormatError = "---Puzzle Size Format Error---";
-   private static final String puzzleSizeRangeError = "---Puzzle Size Range Error (3-10)---";
-   private static final String puzzleDifficultyFormatError = "---Puzzle Difficulty Format Error---";
-   private static final String puzzleDifficultyRangeError = "---Puzzle Difficulty Range Error---";
-   private static final String puzzleSizePrompt = "Enter Puzzle Size in integer form:";
-   private static final String puzzleDifficultyPrompt = "Enter Difficulty Level (1-7):";
-   private static final int  MAX_PUZZLE_SIZE = 10;
-   private static final int MIN_PUZZLE_SIZE = 3;
+    //size errors
+    private static final String puzzleSizeRangeError = "---Puzzle Size Range Error (3-10)---";
+
+    //difficulty errors
+    private static final String puzzleDifficultyRangeError = "---Puzzle Difficulty Range Error (1-7)---";
+
+    //amount errors
+    private static final String puzzleAmountRangeError = "---Puzzle Difficulty Range Error (1-1000)---";
+
+    //prompts
+    private static final String puzzleSizePrompt = "Enter Puzzle Size in integer form:";
+    private static final String puzzleDifficultyPrompt = "Enter Difficulty Level (1-7):";
+    private static final String puzzleAmountPrompt = "Enter Amount of levels to generate:";
+
+    //constant format error
+    private static final String formatError = "--!--Response Formatting Error--!--";
+    //range constants
+    private static final int MAX_PUZZLE_SIZE = 10;
+    private static final int MIN_PUZZLE_SIZE = 3;
+    private static final int MAX_PUZZLE_DIFF = 7;
+    private static final int MIN_PUZZLE_DIFF = 1;
+    private static final int MAX_PUZZLE_AMOUNT = 1000;
+    private static final int MIN_PUZZLE_AMOUNT = 1;
     /*
      * Prompts user through scanner for integer input.
      */
@@ -26,24 +41,35 @@ public class Main {
         int amountVal;
         Scanner mainScanner=new Scanner(System.in);
 
-        while (!sizeCorrect) {
-            System.out.println(puzzleSizePrompt);
-            String inputedSizeString=mainScanner.nextLine();
-            try
-            {
-                sizeVal = Integer.parseInt(inputedSizeString);
-                if (sizeVal <= MAX_PUZZLE_SIZE && sizeVal >= MIN_PUZZLE_SIZE) {
-                    sizeCorrect = true;
+        sizeVal = promptUser(sizeCorrect, mainScanner, puzzleSizePrompt, MAX_PUZZLE_SIZE, MIN_PUZZLE_SIZE, puzzleSizeRangeError, formatError);
+        difficultyVal = promptUser(difficultyCorrect, mainScanner, puzzleDifficultyPrompt, MAX_PUZZLE_DIFF, MIN_PUZZLE_DIFF, puzzleDifficultyRangeError, formatError);
+        amountVal = promptUser(amountCorrect, mainScanner, puzzleAmountPrompt, MAX_PUZZLE_AMOUNT, MIN_PUZZLE_AMOUNT, puzzleAmountRangeError, formatError);
+
+        for (int i =0;  i<amountVal; i++){
+            Puzzle currentPuzzle = new Puzzle(sizeVal, difficultyVal);
+            currentPuzzle.initiate();
+            currentPuzzle.randomize();
+            currentPuzzle.print();
+        }
+    }
+
+    private static int promptUser(boolean correctBoolean, Scanner mainScanner, String prompt, int maxVal, int minVal, String rangeError, String formatError) {
+        int value;
+        while (!correctBoolean) {
+            System.out.println(prompt);
+            String inputString = mainScanner.nextLine();
+            try {
+                value = Integer.parseInt(inputString);
+                if (value <= maxVal && value >= minVal) {
+                    correctBoolean = true;
+                    return value;
+                } else {
+                    System.out.println(rangeError);
                 }
-                else {
-                    System.out.println(puzzleSizeRangeError);
-                }
-            }
-            catch (NumberFormatException nfe) {
-                System.out.println(puzzleSizeFormatError);
+            } catch (NumberFormatException nfe) {
+                System.out.println(formatError);
             }
         }
-            System.out.println("Enter Difficulty Level (1-7):");
-        //prompt user for how many
+        return 0;
     }
 }
